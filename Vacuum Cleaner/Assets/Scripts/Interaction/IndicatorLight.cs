e3f98ad260c76c42428f24daa2100a0d038772fd
Jakob <jakob.hust@gmail.com>
2020-08-06 14:26:13 +0200

    Added indicator light code

---
+++ Vacuum Cleaner/Assets/Scripts/Interaction/IndicatorLight.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class IndicatorLight : MonoBehaviour, IIndicator
+{
+    public Color offColor = Color.red, onColor = Color.green;
+    public Light lightSource;
+
+    private void Start()
+    {
+        if (lightSource == null)
+            lightSource = GetComponent<Light>();
+    }
+
+    public void IndicatorOn()
+    {
+        if (lightSource != null)
+            lightSource.enabled = true;
+    }
+
+    public void IndicatorOff()
+    {
+        if (lightSource != null)
+            lightSource.enabled = false;
+    }
+}
