f02dd752dea887483a5e5f03fb7bfa7610aaff79
Jakob <jakob.hust@gmail.com>
2020-08-06 15:10:15 +0200

    Added vacuum on sound

--- Vacuum Cleaner/Assets/Scripts/Interaction/IndicatorLight.cs
+++ Vacuum Cleaner/Assets/Scripts/Interaction/IndicatorLight.cs
@@ -3,8 +3,10 @@
 public class IndicatorLight : MonoBehaviour, IIndicator
 {
     public Color offColor = Color.red, onColor = Color.green;
-    public bool startOn;
+    public bool startOn, playAudioSource;
     public Light lightSource;
+
+    private AudioSource _audioSource;
 
     private void Start()
     {
@@ -14,17 +16,25 @@ public class IndicatorLight : MonoBehaviour, IIndicator
         if (lightSource != null)
             lightSource.color = startOn ? onColor : offColor;
 
+        if (playAudioSource)
+            _audioSource = GetComponent<AudioSource>();
     }
 
     public void IndicatorOn()
     {
         if (lightSource != null)
             lightSource.color = onColor;
+
+        if (playAudioSource)
+            _audioSource?.Play();
     }
 
     public void IndicatorOff()
     {
         if (lightSource != null)
             lightSource.color = offColor;
+
+        if (playAudioSource)
+            _audioSource?.Stop();
     }
 }
