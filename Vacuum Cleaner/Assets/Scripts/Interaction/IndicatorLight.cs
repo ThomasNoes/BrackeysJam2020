ad5acff1a1803690639489a82e3909b7b8027ae8
Jakob <jakob.hust@gmail.com>
2020-08-06 14:42:32 +0200

    Added light indicator

--- Vacuum Cleaner/Assets/Scripts/Interaction/IndicatorLight.cs
+++ Vacuum Cleaner/Assets/Scripts/Interaction/IndicatorLight.cs
@@ -3,23 +3,28 @@
 public class IndicatorLight : MonoBehaviour, IIndicator
 {
     public Color offColor = Color.red, onColor = Color.green;
+    public bool startOn;
     public Light lightSource;
 
     private void Start()
     {
         if (lightSource == null)
             lightSource = GetComponent<Light>();
+
+        if (lightSource != null)
+            lightSource.color = startOn ? onColor : offColor;
+
     }
 
     public void IndicatorOn()
     {
         if (lightSource != null)
-            lightSource.enabled = true;
+            lightSource.color = onColor;
     }
 
     public void IndicatorOff()
     {
         if (lightSource != null)
-            lightSource.enabled = false;
+            lightSource.color = offColor;
     }
 }
