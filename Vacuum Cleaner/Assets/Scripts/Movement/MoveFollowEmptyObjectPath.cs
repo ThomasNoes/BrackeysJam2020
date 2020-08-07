44d6a1c721a56c3325fdc2695451fefed24b4c78
LasseAafeldt <superlla@hotmail.com>
2020-08-07 16:33:46 +0200

    moving obstacles level

---
+++ Vacuum Cleaner/Assets/Scripts/Movement/MoveFollowEmptyObjectPath.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveFollowEmptyObjectPath : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+
+    private Transform[] path;
+    private int _currentCheckpointIndex = 1;
+    private float checkpointErrorMargin = .1f;
+
+    private void Start()
+    {
+        path = GetComponentsInChildren<Transform>();
+        path[0] = null; //remove the parent object from the array
+    }
+
+    private void FixedUpdate()
+    {
+        MoveWithPath(speed);    
+    }
+
+    private void UpdateNextCheckpoint()
+    {
+        Debug.Log("Path = " + path.Length);
+        if(_currentCheckpointIndex + 1 >= path.Length)
+        {
+            _currentCheckpointIndex = 1; //return first checkpoint position. 
+        }
+        else
+        {
+            _currentCheckpointIndex += 1;
+            
+        }
+    }
+
+    private void MoveWithPath(float speed)
+    {
+        Vector3 checkpoint = GetTargetPosition(path[_currentCheckpointIndex]);
+        float dist = (checkpoint - transform.position).magnitude;
+
+        if (dist <= checkpointErrorMargin)
+        {
+            UpdateNextCheckpoint();
+        }
+        else
+        {
+            if(dist <= speed * Time.deltaTime)
+            {
+                UpdateNextCheckpoint();
+            }
+        }
+        Vector3 dir = GetTargetPosition(path[_currentCheckpointIndex]) + transform.position - transform.position;
+        transform.Translate(dir * speed * Time.deltaTime);
+    }
+
+    private Vector3 GetTargetPosition(Transform obj)
+    {
+        Vector3 targetPos = obj.transform.localPosition;
+        return targetPos;
+    }
+}
---
+++ Vacuum Cleaner/Assets/Scripts/Point.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Point : MonoBehaviour
+{
+    [SerializeField] private float gizmoSize = .5f;
+    private void OnDrawGizmos()
+    {
+        PointGizmo();
+    }
+    private void PointGizmo()
+    {
+        Gizmos.color = Color.black;
+        Gizmos.DrawCube(transform.position, Vector3.one * gizmoSize);
+    }
+}
